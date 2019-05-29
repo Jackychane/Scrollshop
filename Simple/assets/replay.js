8ae3690e1de9e2824e4e64fa0fc624367d5a38a2
Jacky CHANE-KAM <j.chanekam@gmail.com>
2019-05-29 10:39:38 +0200

    premier commit

---
+++ Simple/assets/replay.js
@@ -0,0 +1,7 @@
+
+(function(w,d,n,u,o,t,m){w['SrecObject']=o;w[o]=w[o]||function(){
+(w[o].q=w[o].q||[]).push(arguments)},w[o].l=1*new Date();t=d.createElement(n),
+m=d.getElementsByTagName(n)[0];t.async=1;t.src=u;m.parentNode.insertBefore(t,m)
+})(window,document,'script','https://replayapp.tk/collect/initialize.js','srec');
+srec('init', 'fbd4cb00-7f0d-11e9-a0cf-f291d7827f05');
+    
